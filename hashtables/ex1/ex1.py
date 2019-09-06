f70b23b4f4f114b2dd11b1d862dc463be8d33f7b
Phil Hart <philhart23@gmail.com>
2019-09-06 09:59:39 -0600

    ex1 completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,7 +8,34 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    #Loop through
 
+    for i in range(length):
+        
+        # weight is the weight at i
+        weight = weights[i]
+
+
+        # weight_limit is the limit minus the weight
+
+        weight_limit = limit - weight
+
+        # retrieve the weight_limit and set it as index_pair
+
+        index_pair = hash_table_retrieve(ht, weight_limit)
+
+        # if index_pair is not None and higher than i it'll be index_pair first, else it'll be i first
+
+        if index_pair is not None:
+             group = (index_pair, i) if i < index_pair else (i, index_pair)
+
+            # return the group once we know what order it should go in 
+             return group
+
+         # finally insert everything into the hash table   
+         #  
+        hash_table_insert(ht, weight, i)
+       
 
     return None
 
